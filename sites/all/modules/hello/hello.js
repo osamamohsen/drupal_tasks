ccaa561b344cdd842c41c2f6f9236798d559cf4f
osamamohsen <osamamohsen1994@gmail.com>
2016-12-06 10:09:43 +0200

    add new file

---
+++ sites/all/modules/hello/hello.js
@@ -0,0 +1,9 @@
+/**
+ * Created by osamamohsen on 05/12/16.
+ */
+jQuery(document).ready(function() {
+    // alert("here");
+    jQuery("#submitAjaxButton").click(function () {
+        alert("you Clicked on button");
+    });
+});
